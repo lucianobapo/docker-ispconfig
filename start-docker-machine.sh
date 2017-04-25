6e81c42abcce2c5b2cb5b381863d8ad816bc126d
Luciano Porto <luciano.bapo@gmail.com>
2017-04-25 10:33:31 -0300

    automatic-commit-alias

--- start-docker-machine.sh
+++ start-docker-machine.sh
@@ -23,7 +23,7 @@
 #rsync -rvztPhe "ssh docker@$(docker-machine ip ispconfig-machine)" /home/luciano/code/docker-ispconfig/ $(docker-machine ip ispconfig-machine):/home/docker/docker-ispconfig/
 #ping $(docker-machine ip ispconfig-machine)
 git cmt
-docker-machine ssh ispconfig-machine "git clone https://github.com/lucianobapo/docker-ispconfig.git && cd ~/docker-ispconfig | docker-compose down && docker-compose up -d"
+docker-machine ssh ispconfig-machine "git clone https://github.com/lucianobapo/docker-ispconfig.git -q && cd ~/docker-ispconfig && git pull && docker-compose down && docker-compose up -d"
 #if [ ! -f /home/luciano/code/docker-ispconfig ]; then
 #pwd
 #fi
