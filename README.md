56a8b1a635c8209a1ffaed50766c13e042b4baf1
Luciano Porto <luciano.bapo@gmail.com>
2017-04-25 10:27:22 -0300

    automatic-commit-alias

--- README.md
+++ README.md
@@ -10,6 +10,5 @@ docker rmi $(docker images |awk '{print $3}')
 
 docker volume rm $(docker volume ls |awk '{print $2}')
 
-git cmt && docker-machine ssh \
-ispconfig-machine "cd ~/docker-ispconfig  && git pull && docker-compose down && docker-compose up -d"
+git cmt && docker-machine ssh ispconfig-machine "cd ~/docker-ispconfig  && git pull && docker-compose down && docker-compose up -d"
 
---
+++ start-docker-machine.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# DOCKERPASS=$(openssl rand -base64 32)
+# echo "ROOT password : $DOCKERPASS"
+# echo "root:pass"|chpasswd
+# sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+#if [ ! -z "$DEFAULT_EMAIL_HOST" ]; then
+#sed -i "s/^\(DEFAULT_EMAIL_HOST\) = .*$/\1 = '$MAILMAN_EMAIL_HOST'/g" /etc/mailman/mm_cfg.py
+#newlist -q mailman $(MAILMAN_EMAIL) $(MAILMAN_PASS)
+#newaliases
+#fi
+#echo "START"
+#if [ ! -z "$LANGUAGE" ]; then
+#sed -i "s/^language=en$/language=$LANGUAGE/g" /tmp/ispconfig3_install/install/autoinstall.ini
+#fi
+#if [ ! -z "$COUNTRY" ]; then
+#sed -i "s/^ssl_cert_country=AU$/ssl_cert_country=$COUNTRY/g" /tmp/ispconfig3_install/install/autoinstall.ini
+#fi
+#if [ ! -z "$HOSTNAME" ]; then
+#sed -i "s/^hostname=server1.example.com$/hostname=$HOSTNAME/g" /tmp/ispconfig3_install/install/autoinstall.ini
+#fi
+# php -q /tmp/ispconfig3_install/install/install.php --autoinstall=/tmp/ispconfig3_install/install/autoinstall.ini
+#/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+#rsync -rvztPhe "ssh docker@$(docker-machine ip ispconfig-machine)" /home/luciano/code/docker-ispconfig/ $(docker-machine ip ispconfig-machine):/home/docker/docker-ispconfig/
+#ping $(docker-machine ip ispconfig-machine)
+git cmt
+#docker-machine ssh ispconfig-machine
+if [ ! -f /home/luciano/code/docker-ispconfig ]; then pwd fi
