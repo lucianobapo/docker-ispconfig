8506e74c3a50053a8e410601c94d4492b35ce0e3
Luciano Porto <luciano.bapo@gmail.com>
2017-04-25 17:16:40 -0300

    automatic-commit-alias

--- restart-docker-compose.sh
+++ restart-docker-compose.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker-machine ssh ispconfig-machine "cd /home/docker/docker-ispconfig && docker-compose down"
-git cmt || true && docker-machine ssh ispconfig-machine "cd /home/docker/docker-ispconfig && git pull || true && docker-compose up -d"
+git cmt || true && docker-machine ssh ispconfig-machine "cd /home/docker/docker-ispconfig && git pull || true && docker-compose up -d --build"
 #docker-machine ssh ispconfig-machine docker exec ispconfig service nginx restart
 #docker-machine ssh ispconfig-machine docker exec ispconfig ps -ef
 #docker-machine ssh ispconfig-machine docker exec ispconfig netstat -atunp
