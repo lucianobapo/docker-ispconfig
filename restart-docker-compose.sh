7bda3e6478621c7703c5dd8620f918254159c3f8
Luciano Porto <luciano.bapo@gmail.com>
2017-04-26 02:27:12 -0300

    automatic-commit-alias

--- restart-docker-compose.sh
+++ restart-docker-compose.sh
@@ -4,11 +4,11 @@
 git cmt
 docker-machine ssh ispconfig-machine "if [ -d /home/docker/docker-ispconfig/.git ]; then cd /home/docker/docker-ispconfig && git pull; fi"
 docker-machine ssh ispconfig-machine "if [ ! -d /home/docker/docker-ispconfig ]; then cd /home/docker && git clone https://github.com/lucianobapo/docker-ispconfig.git; fi"
-
-## prepare compose
-docker-machine ssh ispconfig-machine "if [ ! -d /usr/local/bin/docker-compose ]; then sudo curl -o /usr/local/bin/docker-compose -L 'https://github.com/docker/compose/releases/download/1.12.0/docker-compose-$(uname -s)-$(uname -m)'; fi"
-docker-machine ssh ispconfig-machine "if [ -d /usr/local/bin/docker-compose ]; then sudo chmod ugo+x /usr/local/bin/docker-compose; fi"
-
+#
+### prepare compose
+docker-machine ssh ispconfig-machine "if [ ! -f /usr/local/bin/docker-compose ]; then sudo curl -o /usr/local/bin/docker-compose -L 'https://github.com/docker/compose/releases/download/1.12.0/docker-compose-$(uname -s)-$(uname -m)'; fi"
+docker-machine ssh ispconfig-machine "if [ -f /usr/local/bin/docker-compose ]; then sudo chmod ugo+x /usr/local/bin/docker-compose; fi"
+#
 docker-machine ssh ispconfig-machine "cd /home/docker/docker-ispconfig && docker-compose down"
 docker-machine ssh ispconfig-machine "cd /home/docker/docker-ispconfig && docker-compose up -d --build"
 #docker-machine ssh ispconfig-machine docker exec ispconfig service nginx restart
