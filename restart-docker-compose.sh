5edcdbd2f8e12de9dfaed1cad137511d675bdd34
Luciano Porto <luciano.bapo@gmail.com>
2017-04-26 02:11:27 -0300

    automatic-commit-alias

--- restart-docker-compose.sh
+++ restart-docker-compose.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker-machine ssh ispconfig-machine "cd /home/docker/docker-ispconfig && docker-compose down"
+docker-machine ssh ispconfig-machine "if [ ! -d /usr/local/bin/docker-compose ]; then sudo curl -o /usr/local/bin/docker-compose -L 'https://github.com/docker/compose/releases/download/1.12.0/docker-compose-$(uname -s)-$(uname -m)'; fi"
 git cmt || true && docker-machine ssh ispconfig-machine "cd /home/docker/docker-ispconfig && git pull || true && docker-compose up -d --build"
 #docker-machine ssh ispconfig-machine docker exec ispconfig service nginx restart
 #docker-machine ssh ispconfig-machine docker exec ispconfig ps -ef
--- start.sh
+++ start.sh
@@ -23,6 +23,8 @@ fi
 service nginx restart
 service mysql restart
 
+if [ -d /usr/local/bin/docker-compose ]; then pwd; fi
+
 if [ ! -f /usr/local/ispconfig/interface/lib/config.inc.php ]; then
 	php -q /tmp/ispconfig3_install/install/install.php --autoinstall=/tmp/ispconfig3_install/install/autoinstall.ini
 	killall apache2
